5801078a91327031baf6001646ddb3f08e6fdc5b
VladimirBalditsyn <48633877+VladimirBalditsyn@users.noreply.github.com>
2019-05-12 03:52:58 +0300

    Add files via upload

--- ps_store_db.sql
+++ ps_store_db.sql
@@ -201,7 +201,7 @@ insert into PURCHASE values (47, 19, 22, null, 6, 2799.9, '2019-05-02 04:03:09')
 
 /* Пункт 5 */
 
-/* 1) Хотим: узнать, какие скидки не были интересны пользователям */
+/* 1) Хотим узнать, какие скидки не были интересны пользователям */
 select t.sale_nm
 from
   (select sl.sale_nm
@@ -211,7 +211,7 @@ from
     from SALE sl_join
     inner join PURCHASE P on sl_join.sale_id = P.sale_id) t;
 
-/* 2) Хотим: узнать, в какой период каждая из игр в магазине стоила меньше всего
+/* 2) Хотим узнать, в какой период каждая из игр в магазине стоила меньше всего
  (для всех пользователей) */
 select distinct
   game_nm,
@@ -231,4 +231,176 @@ from
   inner join
     SALE S on SXG.sale_id = S.sale_id) t;
 
+/*3)Хотим узнать, в какие дни недели покупатели наиболее активны*/
+with days_purc as (
+  select (case day_num
+  when 0 then 'Воскресенье'
+  when 1 then 'Понедельник'
+  when 2 then 'Вторник'
+  when 3 then 'Среда'
+  when 4 then 'Четверг'
+  when 5 then 'Пятница'
+  when 6 then 'Суббота'
+  end ) as active_days,
+       count(transaction_id) as purcs
+  from (
+     select extract(dow from p.time_of_transaction_dttm) as day_num,
+            p.transaction_id
+     from PURCHASE p
+       ) t
+  group by t.day_num )
+select active_days
+from
+    days_purc
+where purcs = (select max(purcs) from days_purc);
 
+/*4) Хотим узнать игры, пользующиеся наибольшей популярностью у пользователей старше 16 лет*/
+with advanced_purch as (select game_nm, count(transaction_id) as purchs
+  from GAME G
+  inner join PURCHASE P on G.game_id = P.game_id
+  inner join USERS U on P.user_id = U.user_id
+  where date_part('years', age(U.birth_dt)) >= 16
+  group by game_nm)
+select game_nm
+from advanced_purch
+where purchs = (select max(purchs) from advanced_purch);
+
+/*5) Хотим узнать, какие пользователи совешают покупки в основном по специальным предложениям*/
+with by_sp_of as (
+  select user_nm, P.user_id, count(transaction_id) as purch
+  from USERS
+  inner join PURCHASE P on USERS.user_id = P.user_id
+  where special_offer_id is not null
+  group by P.user_id, user_nm
+),
+without_sp_of as (
+  select user_nm, P.user_id, count(transaction_id) as purch
+  from USERS
+  inner join PURCHASE P on USERS.user_id = P.user_id
+  where special_offer_id is null
+  group by P.user_id, user_nm
+)
+select user_nm, user_id
+from
+  by_sp_of b
+where b.user_id in (select user_id from without_sp_of)
+  and (select purch from by_sp_of bs where bs.user_id = b.user_id)
+  >= (select purch from without_sp_of ws where ws.user_id = b.user_id)
+  or  b.user_id not in (select user_id from without_sp_of);
+
+
+/*Пункт 6*/
+select bank_card_num
+from USERS;
+
+insert into USERS values (40, 'crud-rud', '1967-02-20', '4205-5459-6562-6552');
+
+update USERS set user_nm = 'crud-rude' where user_id = 40;
+
+delete from USERS where user_nm = 'crud-rude';
+
+
+select sale_nm
+from SALE;
+
+insert into SALE values (65, 'Session is coming', '2019-05-13', '2019-06-26');
+
+update SALE set sale_end_dttm = '2019-05-26' where sale_nm = 'Session is coming';
+
+delete from SALE where sale_id = 65;
+
+/*Пункт 7*/
+create or replace view users_v as
+  select user_nm,
+         birth_dt::date,
+         substring(bank_card_num from 1 for 5) || '****-****-**' || substring(bank_card_num from 18 for 20) as bank_card
+  from USERS;
+
+select user_nm from users_v;
+
+create or replace view game_v as
+  select game_nm,
+         description_txt,
+         genre_nm,
+         game_full_price_atm,
+         pegi
+  from GAME;
+
+select game_nm, game_full_price_atm from game_v;
+
+create or replace view sale_v as
+  select sale_nm,
+         sale_start_dttm,
+         sale_end_dttm
+  from SALE;
+
+select * from sale_v;
+
+create or replace view special_offer_v as
+  select special_offer_nm,
+         user_id,
+         special_offer_start_dttm,
+         special_offer_end_dttm
+  from SPECIAL_OFFER;
+
+select * from special_offer_v;
+
+create or replace view studio_v as
+  select studio_nm
+  from STUDIO;
+
+select studio_nm from studio_v;
+
+create or replace view purchase_v as
+  select time_of_transaction_dttm,
+         user_id,
+         game_id,
+         game_final_price_amt,
+         sale_id,
+         special_offer_id
+  from PURCHASE;
+
+select user_id, game_id, game_final_price_amt from purchase_v;
+
+/*Следующие три представления созданы исключительно из требования задания,
+т.к прикладной пользы они не особо несут, т.к состоят в основном из служебных полей*/
+create or replace view sale_x_game_v as
+  select *
+  from SALE_X_GAME;
+
+create or replace view special_offer_x_game_v as
+  select *
+  from SPECIAL_OFFER_X_GAME;
+
+create or replace view studio_x_game_v as
+  select *
+  from ps_store.STUDIO_X_GAME;
+
+/*Пункт 8*/
+
+/*Создадим представление, которое помимо основной информации в GAME,
+содержит еще и информацию о студии, выпустившей игру*/
+create or replace view game_advanced_v as
+  select game_nm,
+         game_full_price_atm,
+         description_txt,
+         genre_nm,
+         pegi,
+         studio_nm
+  from GAME G
+inner join STUDIO_X_GAME SXG on G.game_id = SXG.game_id
+inner join STUDIO S on SXG.studio_id = S.studio_id
+order by game_nm;
+
+/*Создадим представление, которое помимо основной информации в SALE,
+содержит еще и информацию о играх и их ценах*/
+create or replace view sale_advanced_v as
+  select sale_nm,
+         game_nm,
+         game_price_amt,
+         sale_start_dttm,
+         sale_end_dttm
+  from SALE S
+inner join SALE_X_GAME SXG on S.sale_id = SXG.sale_id
+inner join GAME G on SXG.game_id = G.game_id
+order by sale_nm;
